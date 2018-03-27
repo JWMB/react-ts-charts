71d10ebd2e3b20effe09d0c64295498da416f1d3
JWMB <jonas.beckeman@gmail.com>
2018-03-27 13:28:40 +0200

    disable serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import './extensions';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+// registerServiceWorker();
